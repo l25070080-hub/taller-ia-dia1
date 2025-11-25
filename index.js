869b259a8672beec2fe63301b4cb7e09d1551794
Jose Sima <Jossysima@gmail.com>
2025-11-25 12:45:29 -0600

    Jose Luis Sima Oy

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Escribe hola mundo en la consola
 console.log("Hola Mundo");
+
+//"¿Qué es un commit y por qué es importante?"
+// Un commit es una instantánea de los cambios realizados en el código fuente de un proyecto. Es una operación fundamental en sistemas de control de versiones como Git. Cada commit registra el estado del proyecto en un momento específico, incluyendo los cambios realizados, quién los hizo y cuándo se hicieron.
+
+//"¿Cuál es la diferencia entre git add y git commit?"
+// git add es el comando que se utiliza para agregar cambios específicos al área de preparación (staging area) antes de hacer un commit. Permite seleccionar qué archivos o partes de archivos se incluirán en el próximo commit.
+
+//"Explícame qué hace el comando git push."
+// git push es el comando que se utiliza para subir los commits locales a un repositorio remoto. Esto permite compartir los cambios realizados en el código con otros colaboradores del proyecto y mantener el repositorio remoto actualizado con los últimos cambios.
